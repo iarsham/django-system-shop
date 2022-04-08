b63bb0a17c92b4fc8eb6e8df6028626b2a041fb4
arsham <aqaarsham@gmail.com>
2022-04-09 01:45:46 +0430

    add session base cart

--- cart/carts.py
+++ cart/carts.py
@@ -53,6 +53,3 @@ class Cart(object):
 
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
-
-    def get_total_items(self):
-        return sum(item['quantity'] for item in self.cart.values())
