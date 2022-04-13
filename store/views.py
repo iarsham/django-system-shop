50e2b8d9acce7ac9882c9c6e5c4339036ca188d5
arsham <aqaarsham@gmail.com>
2022-04-14 02:38:12 +0430

    anonymous comment bug

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Comment
 from .utils import paginate, run_filter
 from cart.forms import AddCartForm
+from django.contrib import messages
 
 
 def category(request):
@@ -22,13 +23,16 @@ def product_detail(request, pk):
     project_detail = Product.objects.get(id=pk)
     product_comments = Comment.objects.all()
     if request.method == "POST":
-        query = request.POST['comment']
-        comment = Comment.objects.create(
-            text_comment=query,
-            reply_product=project_detail,
-            writer=request.user
-        )
-        comment.save()
+        if request.user.is_authenticated:
+            query = request.POST['comment']
+            comment = Comment.objects.create(
+                text_comment=query,
+                reply_product=project_detail,
+                writer=request.user
+            )
+        else:
+            messages.warning(request, 'Please Login To send comment ')
+            return redirect('login')
 
     context = {
         'project_detail': project_detail,
